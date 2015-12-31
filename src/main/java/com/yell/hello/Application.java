2507bf7b0667e855a4b8ee35df4893e07d43c22a
shaunlee0 <shaunlee0@googlemail.com>
2015-12-31 11:50:23 +0000

    initial commit

---
+++ src/main/java/com/yell/hello/Application.java
@@ -0,0 +1,16 @@
+package com.yell.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 45034Sh(Shaun Lee) on 31/12/2015.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+
+}
---
+++ src/main/java/com/yell/hello/Greeting.java
@@ -0,0 +1,24 @@
+package com.yell.hello;
+
+/**
+ * Created by 45034Sh(Shaun Lee) on 31/12/2015.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
